0e40fe2a839a500e995e87823aaf3514c3aff6ad
MeshCollider <dobsonsa68@gmail.com>
2014-08-11 21:36:17 +1200

    Update CoinDefinition.java

--- core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
+++ core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
@@ -244,7 +244,7 @@ public class CoinDefinition {
           "not supported"
     };
     //from main.h: CAlert::CheckSignature
-    public static final String SATOSHI_KEY = "04A9CFD81AF5D53310BE45E6326E706A542B1028DF85D2819D5DE496D8816C83129CE874FE5E3A23B03544BFF35458833779DAB7A6FF687525A4E23CA59F1E2B94";
+    public static final String SATOSHI_KEY = "040182710fa6892d5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08d​ce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9";
     public static final String TESTNET_SATOSHI_KEY = "";
 
     /** The string returned by getId() for the main, production network where people trade things. */
