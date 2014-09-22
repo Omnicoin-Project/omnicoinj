e06837453c339b9291eb5af596c02486eae169c2
MeshCollider <dobsonsa68@gmail.com>
2014-09-22 17:53:01 +1200

    Update AbstractBlockChain.java

--- core/src/main/java/com/google/bitcoin/core/AbstractBlockChain.java
+++ core/src/main/java/com/google/bitcoin/core/AbstractBlockChain.java
@@ -804,7 +804,7 @@ public abstract class AbstractBlockChain {
         checkState(lock.isHeldByCurrentThread());
 		if (storedPrev.getHeight()+1 <= 160) 
 		{ 
-			if(CoinDefinition.startDifficulty != nextBlock.getDifficulty()) 
+			if(CoinDefinition.startDifficulty != nextBlock.getDifficultyTarget()) 
     			throw new VerificationException("Block < 160, Network Bits do not match"); 
 			return;
 		}
